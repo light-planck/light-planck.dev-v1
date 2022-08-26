d8fc00921d1ba82a07ae182fe356d00c67ad9493
light <seseifunc16@gmail.com>
2022-08-26 17:17:46 +0900

    add nav

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,5 +1,28 @@
+import Link from 'next/link';
+import styles from '../styles/nav.module.css';
+
 const Nav = () => {
-  return <p>Nav</p>;
+  return (
+    <nav>
+      <ul className={styles.list}>
+        <li>
+          <Link href="/">
+            <a>Home</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/about">
+            <a>About</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/works">
+            <a>Works</a>
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
 };
 
 export default Nav;
