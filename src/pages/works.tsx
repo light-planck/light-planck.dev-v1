5673957ba4ceb58074905b89df5922f55992ce90
light <seseifunc16@gmail.com>
2022-09-06 17:01:56 +0900

    worksページを追加

---
+++ src/pages/works.tsx
@@ -0,0 +1,13 @@
+import Container from 'components/Container'
+import Hero from 'components/Hero'
+import { NextPage } from 'next'
+
+const works: NextPage = () => {
+  return (
+    <Container>
+      <Hero title="Works" subtitle="更新予定" />
+    </Container>
+  )
+}
+
+export default works
