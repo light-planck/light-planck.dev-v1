4a60fa31e935614c92e7d5499c5eb0e9e8fa25bb
light <seseifunc16@gmail.com>
2023-04-28 17:21:31 +0900

    next/fontでgoogle fontを読み込む

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,17 +1,24 @@
 import Container from './Container'
 import styles from '../styles/footer.module.css'
 import type { NextPage } from 'next'
+import { Open_Sans } from 'next/font/google'
+
+const thisYear = new Date().getFullYear()
+const openSans = Open_Sans({
+  weight: '400',
+  subsets: ['latin'],
+})
 
 const Footer: NextPage = () => {
-  const thisYear = new Date().getFullYear()
-
   return (
     <footer className={styles.wrapper}>
       <Container>
         <div className={styles.flexContainer}>
-          <p className="copyright">
-            <small>&copy;{thisYear} light-planck. All Rights Reserved.</small>
-          </p>
+          <div className={openSans.className}>
+            <p>
+              <small>&copy;{thisYear} light-planck. All Rights Reserved.</small>
+            </p>
+          </div>
         </div>
       </Container>
     </footer>
