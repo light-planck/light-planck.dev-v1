38adc684a4960e66741670ec4e50d9a1477e637d
light <seseifunc16@gmail.com>
2023-05-01 10:34:19 +0900

    navのスタイルをemotionに変更

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,35 +1,36 @@
 import type { NextPage } from 'next'
 import Link from 'next/link'
 
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react'
+const list = css`
+  list-style: none;
+  display: flex;
+  gap: 1.2em;
+  font-size: 1.5em;
+`
+
+const item = css`
+  color: white;
+  text-decoration: none;
+`
+
 const Nav: NextPage = () => {
   return (
     <nav style={{ margin: '2em' }}>
-      <ul
-        style={{
-          listStyle: 'none',
-          display: 'flex',
-          gap: '1.2em',
-          fontSize: '1.5em',
-        }}
-      >
+      <ul css={list}>
         <li>
-          <Link href="/" style={{ color: 'white', textDecoration: 'none' }}>
+          <Link href="/" css={item}>
             Home
           </Link>
         </li>
         <li>
-          <Link
-            href="/about"
-            style={{ color: 'white', textDecoration: 'none' }}
-          >
+          <Link href="/about" css={item}>
             About
           </Link>
         </li>
         <li>
-          <Link
-            href="/posts"
-            style={{ color: 'white', textDecoration: 'none' }}
-          >
+          <Link href="/posts" css={item}>
             Posts
           </Link>
         </li>
