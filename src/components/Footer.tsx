54d9f54b67125ceb1098c7354b7a1b27bc8108f7
light <seseifunc16@gmail.com>
2023-05-01 11:51:40 +0900

    footerのスタイルをemotionに変更

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,20 +1,27 @@
 import type { NextPage } from 'next'
 
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react'
+const container = css`
+  text-align: center;
+  display: flex;
+  justify-content: center;
+  background-color: #343e5f;
+  height: 5em;
+  color: #cccccc;
+`
+const copyRight = css`
+  margin: auto;
+  font-size: 1em;
+`
+
 const thisYear = new Date().getFullYear()
 
 const Footer: NextPage = () => {
   return (
     <footer style={{ marginTop: 'auto' }}>
-      <div
-        style={{
-          display: 'flex',
-          justifyContent: 'center',
-          backgroundColor: '#343E5F',
-          height: '5em',
-          color: '#cccccc',
-        }}
-      >
-        <small style={{ margin: 'auto', fontSize: '1em' }}>
+      <div css={container}>
+        <small css={copyRight}>
           &copy;{thisYear} light-planck. All Rights Reserved.
         </small>
       </div>
