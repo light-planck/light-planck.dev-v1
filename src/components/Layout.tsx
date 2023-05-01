628b9ea89026b670978c4e8fd315efeba94898e5
light <seseifunc16@gmail.com>
2023-05-01 11:53:49 +0900

    layoutのスタイルをemotionに変更

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -4,15 +4,21 @@ import Footer from './Footer'
 import Header from './Header'
 import Div100vh from 'react-div-100vh'
 
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react'
+const container = css`
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+`
+
 interface Props {
   children: ReactNode
 }
 
 const Layout: NextPage<Props> = ({ children }) => {
   return (
-    <Div100vh
-      style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}
-    >
+    <Div100vh css={container}>
       <Header />
       <main>{children}</main>
       <Footer />
