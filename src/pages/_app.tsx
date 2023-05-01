67d98dfa92382206e8d138bfe69a19f1415d0605
light <seseifunc16@gmail.com>
2023-05-01 12:00:03 +0900

    global cssをemotionに変更

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,24 +1,29 @@
-import 'styles/styles.css'
 import type { AppProps } from 'next/app'
-
 import '@fortawesome/fontawesome-svg-core/styles.css'
 import { config } from '@fortawesome/fontawesome-svg-core'
-
 import Layout from 'components/Layout'
 import type { NextPage } from 'next'
 import 'modern-css-reset/dist/reset.min.css'
-
 import { Roboto } from 'next/font/google'
+
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react'
+
+config.autoAddCss = false
+
 const roboto = Roboto({
   weight: '400',
   subsets: ['latin'],
 })
 
-config.autoAddCss = false
+const global = css`
+  background-color: #1e1e1e;
+  color: white;
+`
 
 const MyApp: NextPage<AppProps> = ({ Component, pageProps }) => {
   return (
-    <div className={roboto.className}>
+    <div className={roboto.className} css={global}>
       <Layout>
         <Component {...pageProps} />
       </Layout>
